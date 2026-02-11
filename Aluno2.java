2ac50dc79d95485c77d40d43d7f304d2196a9580
vvankxs7 <cauecpereira06@gmail.com>
2026-02-11 15:01:39 -0300

    Create Aluno2.java

---
+++ Aluno2.java
@@ -0,0 +1,11 @@
+public class Aluno2{
+	String nome;
+	float nota;
+
+	void imprimirNome(){
+		System.out.println("Nome: " +nome);
+	}
+	void imprimirNota(){
+		System.out.println("Nota: " +nota);
+	}
+}
