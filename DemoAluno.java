a7205d1ab55edadc14c13a22be9a8d345ceeeb5d
vvankxs7 <cauecpereira06@gmail.com>
2026-02-11 14:52:11 -0300

    Create DemoAluno.java

---
+++ DemoAluno.java
@@ -0,0 +1,12 @@
+public class DemoAluno{
+	public static void main(String[] args){
+
+	Aluno a = new Aluno();
+
+	a.nome="Caue";
+	a.nota=8.5f;
+
+	System.out.println("Nome: " +a.nome);
+	System.out.println("Nota: " +a.nota);
+	}
+}
